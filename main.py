00512b4a46bf0cf207e93d28017fab4156ea35c5
charlescaseymartin <charlescaseymartin@gmail.com>
2024-05-19 10:55:14 +0200

    clean up

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ prog = 'Wordpress To Static'
 description = 'Given a WRX, this program will create a static copy of it\'s website'
 help = 'An exported WRX .xml file to use for copying'
 # data_path = os.path.join(os.getcwd(), 'data')
-# default_output_file = os.path.join(data_path, 'output.json')
 # user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0'
 parser = argparse.ArgumentParser(prog=prog, description=description)
 parser.add_argument('-w', '--wrx', required=False, nargs=1, help=help)
@@ -26,31 +25,45 @@ def parse_arg():
     return wrx_file
 
 
-def extract_wrx_page_links(wrx_path: str):
+def get_wrx_pages(wrx_path: str):
+    pages = []
     tree = ET.parse(wrx_path)
     channel = tree.getroot()[0]
     title_key = 'title'
     link_key = 'link'
+    published_key = 'published'
     status_key = '{http://wordpress.org/export/1.2/}status'
-    base_page_title = channel.find(title_key).text
-    base_page_link = channel.find(link_key).text
-    items = []
+    base_title = channel.find(title_key).text
+    base_link = channel.find(link_key).text
+    pages.append({
+        title_key: base_title,
+        published_key: True,
+        link_key: base_link,
+    })
 
-    for item in channel.findall('item'):
-        item_title = item.find(title_key).text
-        item_link = item.find(link_key).text
-        item_status = item.find(status_key).text
-        items.append({
-            title_key: item_title,
-            'status': item_status,
-            link_key: item_link
+    for page in channel.findall('item'):
+        page_title = page.find(title_key).text
+        page_link = page.find(link_key).text
+        page_status = page.find(status_key).text
+        is_published = True if page_status == 'publish' else False
+        pages.append({
+            title_key: page_title,
+            published_key: is_published,
+            link_key: page_link,
         })
 
-    print(f'base page title: {base_page_title}')
-    print(f'base page link: {base_page_link}')
-    print(f'items: {items}')
+    return pages
+
+
+def get_static_pages():
+    pass
+
+
+def main():
+    wrx_file = parse_arg()
+    pages = get_wrx_pages(wrx_file)
+    print(f'pages: {pages}')
 
 
 if __name__ == '__main__':
-    wrx_file = parse_arg()
-    extract_wrx_page_links(wrx_file)
+    main()
